10108e3274e3dfa47948edcd9fd6a6be27fadd74
Shaun Einolf <seinolf@gmail.com>
2022-12-06 11:04:14 -0500

    Update install.sh

--- docker/scripts/install.sh
+++ docker/scripts/install.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 echo "copying WebformScheduledEmailManager.php"
 cp /opt/drupal/WebformScheduledEmailManager.php /opt/drupal/web/modules/contrib/webform/modules/webform_scheduled_email/src
+if [ ! -d "/mnt/s3fs/public" ];then
+  echo "/mnt/s3fs/public does not exist. Creating now"
+  mkdir /mnt/s3fs/public
+  cp -R /opt/drupal/web/sites/default/files/* /mnt/s3fs/public
+  chown -R drupaldocker:drupaldocker /mnt/s3fs/public
+fi
+
+if [ ! -d "/mnt/s3fs/private" ];then
+  echo "/mnt/s3fs/private does not exist. Creating now"
+  mkdir /mnt/s3fs/private
+  chown -R drupaldocker:drupaldocker /mnt/s3fs/private
+fi
+
+rm -rf /opt/drupal/web/sites/default/files
+ln -s /mnt/s3fs/public /opt/drupal/web/sites/default/files
