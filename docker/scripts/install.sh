b621e237ea4299ba8768df5383a892fcdf2f8a66
Shaun Einolf <seinolf@gmail.com>
2022-12-06 10:34:14 -0500

    adding path to copy webform email fix to

--- docker/scripts/install.sh
+++ docker/scripts/install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-echo "hello"
+echo "copying WebformScheduledEmailManager.php"
+cp /opt/drupal/WebformScheduledEmailManager.php /opt/drupal/web/modules/contrib/webform/modules/webform_scheduled_email/src
