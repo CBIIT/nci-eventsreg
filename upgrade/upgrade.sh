a5ccab834f25d1f32cc6bf2c07a357b190bf878d
Shaun Einolf <seinolf@gmail.com>
2025-05-08 11:54:42 -0400

    updating devupgrade upgrade

--- upgrade/upgrade.sh
+++ upgrade/upgrade.sh
@@ -13,8 +13,8 @@ git config --global --add safe.directory /var/www/drupal/web/modules/contrib/sec
 git config --global --add safe.directory /var/www/drupal/web/modules/contrib
 echo "removing swiftmailer, webform_mass_email, security_review, rules integration, rules"
 drush --root=.. pmu swiftmailer webform_mass_email security_review scheduler_rules_integration rules nodeaccess -y
-drush --root=.. en symfony_mailer ckeditor5 -y
 echo "running composer"
+drush --root=.. en symfony_mailer ckeditor5 -y
 composer --working-dir=.. install
 echo "copying fixes for redirect subscriber and element"
 cp RedirectSubscriber.php ../web/modules/contrib/url_redirect/src/EventSubscriber
